10a7b5804e784f0720444763250c995c23f9e3bd
Bakhtiar2000 <bakhtiarfahim360@gmail.com>
2024-06-27 23:49:31 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MainLayout from "./components/layout/MainLayout";
+
+function App() {
+  return (
+    <div className="h-full w-full max-w-7xl mx-auto bg-purple-100">
+      <MainLayout />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/AdminLayout.tsx
@@ -0,0 +1,9 @@
+const AdminLayout = () => {
+  return (
+    <div>
+      <h2>admin</h2>
+    </div>
+  );
+};
+
+export default AdminLayout;
